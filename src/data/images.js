dbe6f0f622b8b5672619a942c58674b754cc7a20
captaincoden <captcoden@gmail.com>
2024-08-07 23:53:02 -0500

    Update images.js

--- src/data/images.js
+++ src/data/images.js
@@ -1,116 +1,164 @@
+// src/data/images.js
 
+// Array of image data for the photography portfolio
 export const images = [
-    {
-      src: '/images/landscape/African Boabab tree pirouettes under the Milky Way 16x24.jpg',
-      title: 'African Boabab tree pirouettes under the Milky Way',
-      date: '',
-      location: '',
-      description: '',
-      category: 'Landscape Portfolio',
-    },
-    {
-      src: '/images/landscape/Deep Sighs at Drangarnir Sea Arch Faroe Islands 20x30.jpg',
-      title: 'Deep Sighs at Drangarnir Sea Arch Faroe Islands',
-      date: '',
-      location: '',
-      description:'',
-      category: 'Landscape Portfolio',
-    },
-    {
-      src: '/images/abstract/aDSC_257 262focus merge.jpg',
-      title: '',
-      category: 'Abstract and Bible Verses',
-    },
-    {
-      src: '/images/abstract/aDSC_3808.jpg',
-      title: '',
-      category: 'Abstract and Bible Verses',
-    },
-    {
-      src: '/images/locations/usa/photo4.jpg',
-      title: 'New York City Skyline',
-      category: 'Galleries by Location',
-      location: 'USA',
-    },
-    {
-      src: '/images/locations/italy/photo1.jpg',
-      title: 'Colosseum in Rome',
-      category: 'Galleries by Location',
-      location: 'Italy',
-    },
-    {
-        src: '/images/landscape/New Zealand Roadside Reflection 24x36.jpg',
-        title: 'New Zealand Roadside Reflection',
-        category: 'Landscape Portfolio',
-      },
-      {
-        src: '/images/landscape/Silent Reflections in Patagonia 24x36.jpg',
-        title: 'Silent Reflections in Patagonia',
-        category: 'Landscape Portfolio',
-      },
-      {
-        src: '/images/landscape/Sweet Evening on the Cape 16x24.jpg',
-        title: 'Sweet Evening on the Cape',
-        category: 'Landscape Portfolio',
-      },
-      {
-        src: '/images/landscape/Welcome to the Woods NH 16x24.jpg',
-        title: 'Welcome to the Woods NH',
-        category: 'Landscape Portfolio',
-      },
-      {
-        src: '/images/landscape/Windy Day Lofoten Islands 16x24.jpg',
-        title: 'Windy Day Lofoten Islands',
-        category: 'Landscape Portfolio',
-      },
-      {
-        src: '/images/wildlife/a Grumpy Buffalo 10x10.jpg',
-        title: 'A Grumpy Buffalo',
-        category: 'Wildlife Portfolio',
+  // Landscape Portfolio
+  {
+    src: '/images/landscape/African Boabab tree pirouettes under the Milky Way 16x24.jpg',
+    title: 'African Boabab tree pirouettes under the Milky Way',
+    date: '05/20/2023', // Optional: Leave empty if not available
+    location: 'Africa', // Optional: Leave empty if not available
+    description: 'Beautiful place', // Optional: Leave empty if not available
+    category: 'Landscape Portfolio',
+  },
+  {
+    src: '/images/landscape/Deep Sighs at Drangarnir Sea Arch Faroe Islands 20x30.jpg',
+    title: 'Deep Sighs at Drangarnir Sea Arch Faroe Islands',
+    date: '', // Optional
+    location: 'Faroe Islands', // Optional
+    description: '', // Optional
+    category: 'Landscape Portfolio',
+  },
+  {
+    src: '/images/landscape/New Zealand Roadside Reflection 24x36.jpg',
+    title: 'New Zealand Roadside Reflection',
+    date: '', // Optional
+    location: '', // Optional
+    description: '', // Optional
+    category: 'Landscape Portfolio',
+  },
+  {
+    src: '/images/landscape/Silent Reflections in Patagonia 24x36.jpg',
+    title: 'Silent Reflections in Patagonia',
+    date: '', // Optional
+    location: '', // Optional
+    description: '', // Optional
+    category: 'Landscape Portfolio',
+  },
+  {
+    src: '/images/landscape/Sweet Evening on the Cape 16x24.jpg',
+    title: 'Sweet Evening on the Cape',
+    date: '', // Optional
+    location: '', // Optional
+    description: '', // Optional
+    category: 'Landscape Portfolio',
+  },
+  {
+    src: '/images/landscape/Welcome to the Woods NH 16x24.jpg',
+    title: 'Welcome to the Woods NH',
+    date: '', // Optional
+    location: '', // Optional
+    description: '', // Optional
+    category: 'Landscape Portfolio',
+  },
+  {
+    src: '/images/landscape/Windy Day Lofoten Islands 16x24.jpg',
+    title: 'Windy Day Lofoten Islands',
+    date: '', // Optional
+    location: '', // Optional
+    description: '', // Optional
+    category: 'Landscape Portfolio',
+  },
 
-      },
-      {
-        src: '/images/wildlife/Antelope Pose 8x10.jpg',
-        title: 'Antelope Pose',
-        category: 'Wildlife Portfolio',
+  // Abstract and Bible Verses
+  {
+    src: '/images/abstract/aDSC_257 262focus merge.jpg',
+    title: '', // Optional
+    date: '', // Optional
+    location: '', // Optional
+    description: '', // Optional
+    category: 'Abstract and Bible Verses',
+  },
+  {
+    src: '/images/abstract/aDSC_3808.jpg',
+    title: '', // Optional
+    date: '', // Optional
+    location: '', // Optional
+    description: '', // Optional
+    category: 'Abstract and Bible Verses',
+  },
 
-      },
-      {
-        src: '/images/wildlife/Big Guy 10x10.jpg',
-        title: 'Big Guy',
-        category: 'Wildlife Portfolio',
-
-      },
-      {
-        src: '/images/wildlife/Diva 14x21.jpg',
-        title: 'Diva',
-        category: 'Wildlife Portfolio',
-
-      },
-      {
-        src: '/images/wildlife/Handsome Man 10x10.jpg',
-        title: 'Handsome Man',
-        category: 'Wildlife Portfolio',
-
-      },
-      {
-        src: '/images/wildlife/Potato Digger 10x10.jpg',
-        title: 'Potato Digger',
-        category: 'Wildlife Portfolio',
-
-      },
-      {
-        src: "/images/wildlife/Snorkel Strollin' Africa 16x24.jpg",
-        title: "Snorkel Strollin' Africa",
-        category: 'Wildlife Portfolio',
-
-      },
-      {
-        src: '/images/wildlife/the Real Pumbaa 8x10.jpg',
-        title: 'The Real Pumbaa 8x10.jpg',
-        category: 'Wildlife Portfolio',
-
-      },
-
-  ];
+  // Wildlife Portfolio
+  {
+    src: '/images/wildlife/a Grumpy Buffalo 10x10.jpg',
+    title: 'A Grumpy Buffalo',
+    date: '', // Optional
+    location: 'Africa', // Optional
+    description: '', // Optional
+    category: 'Wildlife Portfolio',
+  },
+  {
+    src: '/images/wildlife/Antelope Pose 8x10.jpg',
+    title: 'Antelope Pose',
+    date: '', // Optional
+    location: 'Africa', // Optional
+    description: '', // Optional
+    category: 'Wildlife Portfolio',
+  },
+  {
+    src: '/images/wildlife/Big Guy 10x10.jpg',
+    title: 'Big Guy',
+    date: '', // Optional
+    location: 'Africa', // Optional
+    description: '', // Optional
+    category: 'Wildlife Portfolio',
+  },
+  {
+    src: '/images/wildlife/Diva 14x21.jpg',
+    title: 'Diva',
+    date: '', // Optional
+    location: 'Africa', // Optional
+    description: '', // Optional
+    category: 'Wildlife Portfolio',
+  },
+  {
+    src: '/images/wildlife/Handsome Man 10x10.jpg',
+    title: 'Handsome Man',
+    date: '', // Optional
+    location: 'Africa', // Optional
+    description: '', // Optional
+    category: 'Wildlife Portfolio',
+  },
+  {
+    src: '/images/wildlife/Potato Digger 10x10.jpg',
+    title: 'Potato Digger',
+    date: '', // Optional
+    location: 'Africa', // Optional
+    description: '', // Optional
+    category: 'Wildlife Portfolio',
+  },
+  {
+    src: "/images/wildlife/Snorkel Strollin' Africa 16x24.jpg",
+    title: "Snorkel Strollin' Africa",
+    date: '', // Optional
+    location: 'Africa', // Optional
+    description: '', // Optional
+    category: 'Wildlife Portfolio',
+  },
+  {
+    src: '/images/wildlife/the Real Pumbaa 8x10.jpg',
+    title: 'The Real Pumbaa',
+    date: '', // Optional
+    location: 'Africa', // Optional
+    description: '', // Optional
+    category: 'Wildlife Portfolio',
+  },
   
+  // // Galleries by Location
+  // {
+  //   src: '/images/locations/usa/photo4.jpg',
+  //   title: 'New York City Skyline',
+  //   date: '', // Optional
+  //   location: 'USA',
+  //   description: '', // Optional
+  //   category: 'Galleries by Location',
+  // },
+  // {
+  //   src: '/images/locations/faroe/photo1.jpg',
+  //   title: 'Deep Sighs at Drangarnir Sea Arch',
+  //   date: '', // Optional
+  //   location: 'Faroe Islands',
+  //   description: '', // Optional
+  //   category: 'Galleries by Location',
+  // },
+];
